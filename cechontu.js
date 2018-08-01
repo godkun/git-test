8be5231f437696305d97142b82a500e8b5a95fe2
godkun <godkun666@126.com>
2018-08-01 13:43:11 +0800

    kkkkaaaccc

--- cechontu.js
+++ cechontu.js
@@ -1,6 +1,5 @@
 //app.js
 const EventProxy = require('./utils/eventproxy.js');
-<<<<<<< HEAD
 
 const b = 2111;
 
@@ -9,6 +8,4 @@ const v = 111;
 const aaa = 8888;
 
 const l = 000;
-=======
 cosnt kkk = 9999
->>>>>>> 298e0002385a0cc25df3eefbd7837551580dbbf7
