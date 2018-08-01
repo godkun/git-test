8c28d9ff3ea64c24107fd0406dcd0d6c1e5bc1bd
godkun <godkun666@126.com>
2018-08-01 11:51:27 +0800

    kkkkaaaccc

--- cechontu.js
+++ cechontu.js
@@ -11,6 +11,7 @@ const les = require('./utils/les-data');
 const {saInit} = require('./utils/sa_wxmp');
 const b = require('vvv');
 
+const aa = require('zzz')
 
 const EventProxy = require('./utils/eventproxy.js');
 const api = require('./config/api');
