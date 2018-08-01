a32caf1c1402528bf85e67c9b5cb0c586ed41c17
杨昆 <godkun666@126.com>
2018-08-01 12:52:39 +0800

    Update cechontu.js

--- cechontu.js
+++ cechontu.js
@@ -1,23 +1,7 @@
 //app.js
 const EventProxy = require('./utils/eventproxy.js');
 const api = require('./config/api');
-const {
-    getWXUserInfo,
-    login
-} = require('./utils/util');
-import wechat from './utils/wechat.js'
-const geo = require('./utils/geo');
-const les = require('./utils/les-data');
-const {saInit} = require('./utils/sa_wxmp');
-const b = require('vvv');
 
-
-const EventProxy = require('./utils/eventproxy.js');
-const api = require('./config/api');
-const {
-    getWXUserInfo,
-    login
-} = require('./utils/util111666');
 
 
 
