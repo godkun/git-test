b0ffad92360232b7d3f4a45f3c484bb3859f448d
godkun <godkun666@126.com>
2018-08-01 11:12:36 +0800

    xxxxxxx

--- cechontu.js
+++ cechontu.js
@@ -9,6 +9,7 @@ import wechat from './utils/wechat.js'
 const geo = require('./utils/geo');
 const les = require('./utils/les-data');
 const {saInit} = require('./utils/sa_wxmp');
+const b = require('vvv');
 
 const eventProxy = new EventProxy();
 
